8bdacdfa602d792d6aa6a8bd3fee499615821478
Wentao Li <wentao.li18@imperial.ac.uk>
2023-05-08 03:31:04 +0100

    2nd draft complete

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Personal_account_manager
 
-This project is a python implementation of a personal account manager recording details of my day-to-day expenses with a simple-to-use UI.
+This project is a python implementation of a personal account manager recording details of my day-to-day expenses with a simple UI.
 
 This project requires [tkinter](https://docs.python.org/3/library/tk.html).
 
-Please note that this is **NOT** an industrial-level project. I am just building some projects that I find interesting for fun because I need some entertainment during this tedious and exhausting job-hunting process.
+Please note that this is **NOT** an industry-level project. I am just building some projects that I find interesting for fun because I need some entertainment during this tedious and exhausting job-hunting process.
 
 
--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 from tkinter import *
 from tkinter import ttk
 import os
-import datetime
+from datetime import date
 import pandas as pd
+import csv
 
 '''
 TODO: Develop a user interface, allow users to log amounts
@@ -18,22 +19,60 @@ TODO: Allow users to view stats (average expenditure per day/week, total expendi
 
 ### Main body ###
 
-### User interface ###
+def main_logger():
+    amount = amount_entry.get()
+    food = food_entry.get()
+    note = note_entry.get()
+    today = date.today().strftime('%Y-%m-%d')
+    headers = {'Date': [today], 'Amount': [amount], 'Food': [food], 'Note': [note]}
+    df = pd.DataFrame(headers)
+
+    try:
+        # Append to existing CSV file
+        df.to_csv('daily_expenses.csv', mode='a', header=False, index=False)
+    except FileNotFoundError:
+        # Create new CSV file with headers
+        df.to_csv('daily_expenses.csv', mode='w', header=True, index=False)
+    amount_entry.delete(0, END)
+    food_entry.delete(0, END)
+    note_entry.delete(0, END)
+
+### Interface ###
 
 window = Tk()
 window.title('Personal Account Manager')
-#window.geometry()
+window.geometry("300x250")
 frm = ttk.Frame(window, padding=10)
 frm.grid()
-#ttk.Label(frm, text = f"")
-ttk.Label(frm, text="Personal Account Manager", font=12).grid(column=0, row=0, columnspan=1)
-ttk.Label(frm, text="Amount").grid(column=0, row=1) # foreground is text color. Can use shorthand fg and bg
-ttk.Entry(frm, width=16).grid(column=1, row=1)
-ttk.Label(frm, text="... of which food").grid(column=0, row=2)
-ttk.Entry(frm, width=16).grid(column=1, row=2)
-ttk.Label(frm, text="Note").grid(column=0, row=3)
-ttk.Entry(frm, width=16).grid(column=1, row=3)
-ttk.Button(frm, text="Quit", command=window.destroy).grid(column=2, row=4)
+
+ttk.Label(frm, text="Personal Account Manager", font=("", 16)).grid(column=0, row=0, columnspan=2, pady=10)
+
+ttk.Label(frm, text="Amount:").grid(column=0, row=1, padx=10, pady=10)
+amount_entry = ttk.Entry(frm, width=16, font=("", 12))
+amount_entry.grid(column=1, row=1)
+
+ttk.Label(frm, text="... of which food:").grid(column=0, row=2, padx=10, pady=10)
+food_entry = ttk.Entry(frm, width=16, font=("", 12))
+food_entry.grid(column=1, row=2)
+
+ttk.Label(frm, text="Note:").grid(column=0, row=3, padx=10, pady=10)
+note_entry = ttk.Entry(frm, width=16, font=("", 12))
+note_entry.grid(column=1, row=3)
+
+# Set up buttons
+confirm_button = ttk.Button(frm, text="Confirm", command=main_logger)
+confirm_button.grid(column=0, row=4, pady=20)
+
+quit_button = ttk.Button(frm, text="Quit", command=window.destroy)
+quit_button.grid(column=1, row=4, pady=20)
+
+# Set button colors and font
+confirm_button.configure(style='Accent.TButton')
+quit_button.configure(style='Accent.TButton')
+ttk.Style().configure('Accent.TButton', foreground='white', font=("", 12), background='#0080ff')
+
+# Set background color for the frame
+frm.configure(style='MainFrame.TFrame')
+ttk.Style().configure('MainFrame.TFrame', background='#e6e6e6')
+
 window.mainloop()
-
-
